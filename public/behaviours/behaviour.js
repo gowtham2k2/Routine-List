0be80c81792b965751401c068c4ad82b3425fba7
gowtham2k2 <gowthamangowda7@gmail.com>
2024-07-12 22:18:24 +0530

    minor style improvement

--- public/behaviours/behaviour.js
+++ public/behaviours/behaviour.js
@@ -1,5 +1,11 @@
 var loginBtn = document.getElementById("loginBtn");
 var loginForm = document.getElementById("loginForm");
+var cancelLogin = document.getElementById("cancelLoginBtn");
+
 loginBtn.addEventListener("click", () => {
   loginForm.removeAttribute("hidden");
+});
+
+cancelLogin.addEventListener("click", () => {
+  loginForm.setAttribute("hidden", true);
 });
